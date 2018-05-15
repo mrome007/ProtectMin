c7817a944371fff01336ef6acffd5e6a633c39d2
mrome007 <mrome007@ucr.edu>
2018-05-14 19:32:59 -0700

    Populate Object pool at start. For now.

--- Assets/Scripts/MinsPool.cs
+++ Assets/Scripts/MinsPool.cs
@@ -10,19 +10,50 @@ public class MinsPool : MonoBehaviour
     [SerializeField]
     private List<MinLight> MinsLightObjects;
 
-    private Dictionary<MinsType, List<GameObject>> MinsPoolContainer;
+    [SerializeField]
+    private int NumberOfObjectsPerPool = 10;
 
-	private void Start()
+    [SerializeField]
+    private Transform MinPoolRoot;
+
+    private Dictionary<MinsType, List<MinLight>> MinsPoolContainer;
+
+    private void Awake()
     {
-      
+        MinsPoolContainer = new Dictionary<MinsType, List<MinLight>>();
+    }
+
+    private void Start()
+    {
+        PopulatePool();
+    }
+
+    public void PopulatePool()
+    {
+        foreach(var min in MinsMainObjects)
+        {
+            if(!MinsPoolContainer.ContainsKey(min.MinType))
+            {
+                MinsPoolContainer.Add(min.MinType, new List<MinLight>());
+                for(int count = 0; count < NumberOfObjectsPerPool; count++)
+                {
+                    var minLight = (MinLight)Instantiate(MinsLightObjects[(int)min.MinType]);
+                    minLight.transform.parent = MinPoolRoot;
+                    minLight.transform.localPosition = Vector3.zero;
+                    minLight.Initialize(min);
+                    minLight.gameObject.SetActive(false);
+                    MinsPoolContainer[min.MinType].Add(minLight);
+                }
+            }
+        }
     }
 }
 
 public enum MinsType
 {
-    None,
-    Block,
-    Floor,
-    Jump,
-    Projectile
+    None = 4,
+    Block = 0,
+    Floor = 1,
+    Jump = 2,
+    Projectile = 3
 }
