5a0740514bf54f214b62507f1e7560a3ffbdb91e
mrome007 <mrome007@ucr.edu>
2018-05-16 20:53:42 -0700

    Simple scrolling background. I'm setting it up for the player to not move and everything around it will move instead.

---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform FirstBackground;
+
+    [SerializeField]
+    private Transform SecondBackground;
+
+    [SerializeField]
+    private float outOfCameraLimit;
+
+    [SerializeField]
+    private Transform resetTransform;
+
+    [SerializeField]
+    private float scrollSpeed;
+
+    private void Update()
+    {
+        FirstBackground.transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+        SecondBackground.transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+
+        if(FirstBackground.transform.localPosition.x <= outOfCameraLimit)
+        {
+            FirstBackground.transform.localPosition = resetTransform.localPosition;
+        }
+
+        if(SecondBackground.transform.localPosition.x <= outOfCameraLimit)
+        {
+            SecondBackground.transform.localPosition = resetTransform.localPosition;
+        }
+    }
+}
