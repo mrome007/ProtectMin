b2750dc89a5facac51ac0864b7952ec1c16270b3
mrome007 <mrome007@ucr.edu>
2018-05-20 15:44:43 -0700

    Basic player movement. Move on the x and z axes.

---
+++ Assets/PlayerMinMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMinMovement : MonoBehaviour 
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Vector2 xAxisBoundary;
+
+    [SerializeField]
+    private Vector2 zAxisBoundary;
+
+    private Vector3 movementVector;
+
+    private void Awake()
+    {
+        movementVector = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.z = Input.GetAxis("Vertical");
+
+        transform.Translate(movementVector * movementSpeed * Time.deltaTime);
+
+        //Clamp position;
+        movementVector = transform.position;
+        movementVector.x = Mathf.Clamp(movementVector.x, xAxisBoundary.x, xAxisBoundary.y);
+        movementVector.z = Mathf.Clamp(movementVector.z, zAxisBoundary.x, zAxisBoundary.y);
+
+        transform.position = movementVector;
+    }
+}
